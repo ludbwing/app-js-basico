3e9d03a006969d2903dc9af4dc75c2130c281c98
ludwingaldair25 <ludwingaldairmamaniyucra6@gmail.com>
2024-11-25 13:08:48 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+//  alert("Ludwing Aldair Mamani Yucra");
+
+let nombre = "Ludwing Aldair Mamani Yucra";
+const sigla_senati = "SENATI";
+
+//  sigla_senati = "dkfjhbary";
+
+console.log(sigla_senati);
+console.log(nombre);
+
+//Arrays
+// let edades = [23, 45, 23, 1, 15, 70, 100];
+// for (let i = 0; i < edades.length; i++) {
+//     console.log(edades[i]);
+// }
+// for(item of edades){
+//     console.log(item);
+// }
+// edades.forEach(function(edad){
+//     console.log(edad);
+// });
+
+// edades.map(function(e){
+//     if (e >= 18) {
+//         console.log(e + " es mayor o igual a 18 años.");
+//     } else {
+//         console.log(e + " es menor a 18 años.");
+//     }
+// });
+
+// funcion normal
+//function nombreFuncion(){
+
+//}
+
+// funcion Anónima
+//function nombreFuncion(){
+//}
+
+//EVENTOS EN JS
+let botonHtml = document.getElementById('pruba');
+botonHtml.addEventListener('click',function(e){
+    console.log("Alguien hizo click");
+});
+
+
+//tarea hacer eventos 10 en el archivo js y que sean funcionales  
